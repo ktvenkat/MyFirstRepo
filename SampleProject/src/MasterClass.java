9e4e523142a261dd56fe34d626c7205f7c873a52
thati <thati@192.168.0.182>
2021-12-01 17:56:48 +0530

    master

--- SampleProject/src/MasterClass.java
+++ SampleProject/src/MasterClass.java
@@ -1,5 +1,11 @@
 
 public class MasterClass {
+<<<<<<< Upstream, based on Branch3
 	
+=======
+	public static void main(String[] args) {
+		System.out.println("master");
+	}
+>>>>>>> 3e117b7 added
 
 }
